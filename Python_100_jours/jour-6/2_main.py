080810829b6b70311789de37c58e94fc4a7faf7a
franckdun <scorven7@gmail.com>
2023-09-21 11:46:23 +0200

    update

---
+++ Python_100_jours/jour-6/2_main.py
@@ -0,0 +1,10 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "mark" and password == "password":
+ print("Welcome Mark!")
+elif username == "suzanne":
+ print("Hey there Suzanne!")
+else:
+ print("Go away!")
