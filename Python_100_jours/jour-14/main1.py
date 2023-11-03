39a36247489207659d513f99f7a14d243279feb7
franckdun <scorven7@gmail.com>
2023-11-03 15:02:11 +0100

    update

--- Python_100_jours/jour-14/main1.py
+++ Python_100_jours/jour-14/main1.py
@@ -1,54 +1,208 @@
 #présentation du jeu
-print("PIERRE FEUILLE CISEAUX GAME")
-print()
-print("les mouvement posibles sont; p pour pierre, f pour feuille et c pour ciseaux, puis faite entrer")
+print("PIERRE-FEUILLE-CISEAUX")
+print("Tapez une des lettres ci-dessous puis faites entré.")
+print(" p   Pour pierre.")
+print(" f   Pour feuille.")
+print(" c   Pour ciseaux.")
 print()
 
 #Permet de faire une saisie inconito
 from getpass import getpass as input
 
+#--------- 1er TOUR ---------
+print("Premier tour !")
+
 #déclaration des variables
-player1 = input("Player1 Quel est votre mouvement ?")
-score1 = score1 + 0
-player2 = input("Player2 Quel votre mouvement ?")
-score2 = score2 + 0
+player1 = input("Player1 Quel est votre choix ?")
+player2 = input("Player2 Quel votre choix ?")
+score1 = 0
+score2 = 0
 print()
 
 #Cas d'égalité
 if player1 == "p" and player2 == "p":
-  print("pierre contre pierre, égalité !")
+    print("pierre contre pierre, égalité !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
 elif player1 == "f" and player2 == "f":
-  print("feuille contre feuille, égalité !")
+    print("feuille contre feuille, égalité !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
 elif player1 == "c" and player2 == "c":
-  print("ciseaux contre ciseaux, égalité !")
+    print("ciseaux contre ciseaux, égalité !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
 
 #Cas gagnant player1
 elif player1 == "p" and player2 == "c":
-  print("pierre bat le ciseaux, player1 Gagné !")
-  score1 = score1 + 1
+    score1 = score1 + 1
+    print("La pierre écrase les ciseaux, player1 Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
 elif player1 == "f" and player2 == "p":
-  print("feuille bat la pierre, player1 Gagné !")
-  score1 = score1 + 1
+    score1 = score1 + 1
+    print("La feuille enveloppe la pierre, player1 Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
 elif player1 == "c" and player2 == "f":
-  print("ciseaux bat la feuille, player1 Gagné !")
-  score1 = score1 + 1
+    score1 = score1 + 1
+    print("Les ciseaux coupent la feuille, player1 Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
 
 #Cas gagnant player2
 elif player1 == "p" and player2 == "f":
-  print("feuille bat la pierre, player2 Gagné !")
-  score2 = score2 + 1
+    score2 = score2 + 1
+    print("La feuille enveloppe la pierre, player2 Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
 elif player1 == "f" and player2 == "c":
-  print("ciseaux bat la feuille, player2 Gagné !")
-  score2 = score2 + 1
+    score2 = score2 + 1
+    print("Les ciseaux coupent la feuille, player2 Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
 elif player1 == "c" and player2 == "p":
-  print("pierre bat le ciseaux, player2 Gagné !")
-  score2 = score2 + 1
+    score2 = score2 + 1
+    print("La pierre écrase les ciseaux, player2 Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+
 else:
-  print("je n'ai pas compris, recommencé")
-  
-#déclaration des variables
-player1 = input("Player1 Quel est votre mouvement ?")
-score1 = score1 
-player2 = input("Player2 Quel votre mouvement ?")
-score2 = score2 
+    print("Je n'ai pas compris, recommencez")
 print()
+
+#--------- DEUXIEME TOUR ---------
+#Présentation
+print("Deuxième tour !")
+if score1 > score2:
+    print("Attention player2 si player1 gagne ce tour, Player1 aura gagné !")
+elif score2 > score1:
+    print("Attention player1 si player2 gagne ce tours, Player2 aura gagné !")
+
+#déclaration des variables
+player1 = input("Player1 Quel est votre choix ?")
+print()
+player2 = input("Player2 Quel votre choix ?")
+print()
+
+#Cas d'égalité
+if player1 == "p" and player2 == "p":
+    print("pierre contre pierre, égalité !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+elif player1 == "f" and player2 == "f":
+    print("feuille contre feuille, égalité !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+elif player1 == "c" and player2 == "c":
+    print("ciseaux contre ciseaux, égalité !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+
+#Cas gagnant player1
+elif player1 == "p" and player2 == "c":
+    score1 = score1 + 1
+    print("La pierre écrase les ciseaux, player1 est Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+elif player1 == "f" and player2 == "p":
+    score1 = score1 + 1
+    print("La feuille enveloppe la pierre, player1 est Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+elif player1 == "c" and player2 == "f":
+    score1 = score1 + 1
+    print("Les ciseaux coupent la feuille, player1 est Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+
+#Cas gagnant player2
+elif player1 == "p" and player2 == "f":
+    score2 = score2 + 1
+    print("La feuille enveloppe la pierre, player2 Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+elif player1 == "f" and player2 == "c":
+    score2 = score2 + 1
+    print("Les ciseaux coupent la feuille, player2 Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+elif player1 == "c" and player2 == "p":
+    score2 = score2 + 1
+    print("La pierre écrase les ciseaux, player2 est Gagnant !")
+    print("Score player1:", score1)
+    print("Score player2:", score2)
+
+else:
+    print("Je n'ai pas compris, recommencez")
+
+#--------- TROIXIEME TOUR ---------
+
+print()
+if (score1 > score2):
+    print("Player1 est Gagnant du match, bravo!")
+elif (score2 > score1):
+    print("Player2 est Gagnant du match, bravo!")
+else:
+    print("Match nul, bien joué !")
+    print()
+    print("Dernier tour !")
+    #déclaration des variables
+    player1 = input("Player1 Quel est votre choix ?")
+    player2 = input("Player2 Quel votre choix ?")
+    print()
+
+    #Cas d'égalité
+    if player1 == "p" and player2 == "p":
+        print("pierre contre pierre, égalité !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+    elif player1 == "f" and player2 == "f":
+        print("feuille contre feuille, égalité !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+    elif player1 == "c" and player2 == "c":
+        print("ciseaux contre ciseaux, égalité !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+
+    #Cas gagnant player1
+    elif player1 == "p" and player2 == "c":
+        score1 = score1 + 1
+        print("La pierre écrase les ciseaux, player1 Gagnant !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+    elif player1 == "f" and player2 == "p":
+        score1 = score1 + 1
+        print("La feuille enveloppe la pierre, player1 Gagnant !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+    elif player1 == "c" and player2 == "f":
+        score1 = score1 + 1
+        print("Les ciseaux coupent la feuille, player1 Gagnant !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+    #Cas gagnant player2
+    elif player1 == "p" and player2 == "f":
+        score2 = score2 + 1
+        print("La feuille enveloppe la pierre, player2 Gagnant !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+    elif player1 == "f" and player2 == "c":
+        score2 = score2 + 1
+        print("Les ciseaux coupent la feuille, player2 Gagnant !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+    elif player1 == "c" and player2 == "p":
+        score2 = score2 + 1
+        print("La pierre écrase les ciseaux, player2 Gagnant !")
+        print("Score player1:", score1)
+        print("Score player2:", score2)
+print()
+print("----- Resultat final -----")
+if (score1 > score2):
+    print("Player1 est le gagnant du match, bravo!")
+elif (score2 > score1):
+    print("Player2 est le gagnant du match, bravo!")
+else:
+    print("Je n'ai pas compris, recommencez")
--- Python_100_jours/jour-14/main2.py
+++ Python_100_jours/jour-14/main2.py
@@ -1,208 +1,42 @@
-#présentation du jeu
-print("PIERRE-FEUILLE-CISEAUX")
-print("Tapez une des lettres ci-dessous puis faites entré.")
-print(" p   Pour pierre.")
-print(" f   Pour feuille.")
-print(" c   Pour ciseaux.")
-print()
-
-#Permet de faire une saisie inconito
+#correction de Replit
 from getpass import getpass as input
 
-#--------- 1er TOUR ---------
-print("Premier tour !")
-
-#déclaration des variables
-player1 = input("Player1 Quel est votre choix ?")
-player2 = input("Player2 Quel votre choix ?")
-score1 = 0
-score2 = 0
+print("E P I C    🪨  📄 ✂️    B A T T L E ")
+print()
+print("Select your move (R, P or S)")
 print()
 
-#Cas d'égalité
-if player1 == "p" and player2 == "p":
-    print("pierre contre pierre, égalité !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "f" and player2 == "f":
-    print("feuille contre feuille, égalité !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "c" and player2 == "c":
-    print("ciseaux contre ciseaux, égalité !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
+player1Move = input("Player 1 > ")
+print()
+player2Move = input("Player 2 > ")
+print()
 
-#Cas gagnant player1
-elif player1 == "p" and player2 == "c":
-    score1 = score1 + 1
-    print("La pierre écrase les ciseaux, player1 Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "f" and player2 == "p":
-    score1 = score1 + 1
-    print("La feuille enveloppe la pierre, player1 Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "c" and player2 == "f":
-    score1 = score1 + 1
-    print("Les ciseaux coupent la feuille, player1 Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-
-#Cas gagnant player2
-elif player1 == "p" and player2 == "f":
-    score2 = score2 + 1
-    print("La feuille enveloppe la pierre, player2 Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "f" and player2 == "c":
-    score2 = score2 + 1
-    print("Les ciseaux coupent la feuille, player2 Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "c" and player2 == "p":
-    score2 = score2 + 1
-    print("La pierre écrase les ciseaux, player2 Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-
+if player1Move=="R":
+  if player2Move=="R":
+    print("You both picked Rock, draw!")
+  elif player2Move=="S":
+    print("Player1 smashed Player2's Scissors into dust with their Rock!")
+  elif player2Move=="P":
+    print("Player1's Rock is smothered by Player2's Paper!")
+  else:
+    print("Invalid Move Player 2!")
+elif player1Move=="P":
+  if player2Move=="R":
+    print("Player2's Rock is smothered by Player1's Paper!")
+  elif player2Move=="S":
+    print("Player1's Paper is cut into tiny pieces by Player2's Scissors!")
+  elif player2Move=="P":
+    print("Two bits of paper flap at each other. Dissapointing. Draw.")
+  else:
+    print("Invalid Move Player 2!")
+elif player1Move=="S":
+  if player2Move=="R":
+    print("Player 2's Rock makes metal-dust out of Player1's Scissors")
+  elif player2Move=="S":
+    print("Ka-Shing! Scissors bounce off each other like a dodgy sword fight! Draw.")
+  elif player2Move=="P":
+    print("Player1's Scissors make confetti out of Player2's paper!")
+  else:
+    print("Invalid Move Player 2!")
 else:
-    print("Je n'ai pas compris, recommencez")
-print()
-
-#--------- DEUXIEME TOUR ---------
-#Présentation
-print("Deuxième tour !")
-if score1 > score2:
-    print("Attention player2 si player1 gagne ce tour, Player1 aura gagné !")
-elif score2 > score1:
-    print("Attention player1 si player2 gagne ce tours, Player2 aura gagné !")
-
-#déclaration des variables
-player1 = input("Player1 Quel est votre choix ?")
-print()
-player2 = input("Player2 Quel votre choix ?")
-print()
-
-#Cas d'égalité
-if player1 == "p" and player2 == "p":
-    print("pierre contre pierre, égalité !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "f" and player2 == "f":
-    print("feuille contre feuille, égalité !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "c" and player2 == "c":
-    print("ciseaux contre ciseaux, égalité !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-
-#Cas gagnant player1
-elif player1 == "p" and player2 == "c":
-    score1 = score1 + 1
-    print("La pierre écrase les ciseaux, player1 est Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "f" and player2 == "p":
-    score1 = score1 + 1
-    print("La feuille enveloppe la pierre, player1 est Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "c" and player2 == "f":
-    score1 = score1 + 1
-    print("Les ciseaux coupent la feuille, player1 est Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-
-#Cas gagnant player2
-elif player1 == "p" and player2 == "f":
-    score2 = score2 + 1
-    print("La feuille enveloppe la pierre, player2 Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "f" and player2 == "c":
-    score2 = score2 + 1
-    print("Les ciseaux coupent la feuille, player2 Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-elif player1 == "c" and player2 == "p":
-    score2 = score2 + 1
-    print("La pierre écrase les ciseaux, player2 est Gagnant !")
-    print("Score player1:", score1)
-    print("Score player2:", score2)
-
-else:
-    print("Je n'ai pas compris, recommencez")
-
-#--------- TROIXIEME TOUR ---------
-
-print()
-if (score1 > score2):
-    print("Player1 est Gagnant du match, bravo!")
-elif (score2 > score1):
-    print("Player2 est Gagnant du match, bravo!")
-else:
-    print("Match nul, bien joué !")
-    print()
-    print("Dernier tour !")
-    #déclaration des variables
-    player1 = input("Player1 Quel est votre choix ?")
-    player2 = input("Player2 Quel votre choix ?")
-    print()
-
-    #Cas d'égalité
-    if player1 == "p" and player2 == "p":
-        print("pierre contre pierre, égalité !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-    elif player1 == "f" and player2 == "f":
-        print("feuille contre feuille, égalité !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-    elif player1 == "c" and player2 == "c":
-        print("ciseaux contre ciseaux, égalité !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-
-    #Cas gagnant player1
-    elif player1 == "p" and player2 == "c":
-        score1 = score1 + 1
-        print("La pierre écrase les ciseaux, player1 Gagnant !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-    elif player1 == "f" and player2 == "p":
-        score1 = score1 + 1
-        print("La feuille enveloppe la pierre, player1 Gagnant !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-    elif player1 == "c" and player2 == "f":
-        score1 = score1 + 1
-        print("Les ciseaux coupent la feuille, player1 Gagnant !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-    #Cas gagnant player2
-    elif player1 == "p" and player2 == "f":
-        score2 = score2 + 1
-        print("La feuille enveloppe la pierre, player2 Gagnant !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-    elif player1 == "f" and player2 == "c":
-        score2 = score2 + 1
-        print("Les ciseaux coupent la feuille, player2 Gagnant !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-    elif player1 == "c" and player2 == "p":
-        score2 = score2 + 1
-        print("La pierre écrase les ciseaux, player2 Gagnant !")
-        print("Score player1:", score1)
-        print("Score player2:", score2)
-print()
-print("----- Resultat final -----")
-if (score1 > score2):
-    print("Player1 est le gagnant du match, bravo!")
-elif (score2 > score1):
-    print("Player2 est le gagnant du match, bravo!")
-else:
-    print("Je n'ai pas compris, recommencez")
+  print("Invalid Move Player 1!")
