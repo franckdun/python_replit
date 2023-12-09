34e67365f6854afbbbbe3d59aacc114aa8414627
franckdun <scorven7@gmail.com>
2023-12-09 23:40:20 +0100

    update

--- Python_100_jours/jour-19/main4.py
+++ Python_100_jours/jour-19/main4.py
@@ -1,31 +1,30 @@
-# Calculateur de prêt indiquant le montant à rembourser pour un prêt de 1 000 $ avec un TAEG de 5 % sur 10 ans.
+# Critères du crédit
+capital = 1000
+mensualite = 100
+taux_mensuel = 5
 
-# Demander le montant à emprunter sur 10 ans
-emprunte = int(input("Combien souhaitez-vous emprunter sur 10 ans ? "))
+# Initialisation des variables
+total_paiement = 0
+total_mois = 0 
 
-# Calcul du TAEG
-taux = 0.05 # 5% de l'empreint
+# Boucle for pour calculer le nombre de mois de paiement
+for mois in range( 1, 500):  # Utilisation d'une limite pour éviter une boucle infinie au cas où le prêt ne serait jamais remboursé
+    interet_mensuel = (capital/100) * taux_mensuel
+    capital += interet_mensuel
+    total_paiement += interet_mensuel
+    capital -= mensualite
+    total_mois += 1
 
-
-# Calcul du coût du crédit sur 10 ans
-ans = 10 # nombre d'années
-payement = emprunte / ans # calcul du montant pour une années
-
-# Calcul du montant à rembourser la 1re année, TAEG à 0 %
-print(f"Traite n° 1 : {payement:.2f}")
-
-# Boucle for pour calculer le montant de chaque traite sur 10 ans
-total_interet = 0
-totaltraites = 0
-for annee in range(2, ans + 1): #pour chaque annee dans la liste.la liste traite de 1 ans à 10 ans mais en commençant à 2 (car sans la traite de base)
-    interet = payement * taux # interet calculé a chaque tour avec la nouvelle valeur de payement foix le taux
-    payement += interet # 100 + 5 = 105 on ajoute a chaque tour de boucle
-    total_interet += interet # a chaque tour on ajoute le montant de l'interet à la somme total des interets
-    print(f"Payement n° {annee} : {payement:.2f} $ dont TAEG {interet:.2f} $")
-
-    totaltraites += payement #total des payements
-# Calcul du montant des intérets sur 10 ans
-totalinteret = totaltraites - emprunte
-print(f"Cout total crédit : emprunt {emprunte} interets {totalinteret:.2f} total {totaltraites:.2f} $")
+    # Vérifier si le capital est remboursé
+    if capital <= 100:
+        print(f"Mois {mois}:")
+        print(f"Payement {capital + interet_mensuel:.2f}€ / Interets {interet_mensuel:.2f}€ / Solde {capital:.2f}€")
+        break
+      
+    # Afficher le paiement mensuel
+    print(f"Mois {mois}:")
+    print(f"Payement {mensualite}€ / Interets {interet_mensuel:.2f}€ / Solde {capital:.2f}€\n")
 
 
+# Afficher le résultat final
+print(f"\n{total_mois} mois pour rembourser, cout du credit {total_paiement:.2f}€")
---
+++ Python_100_jours/jour-19/main6.py
@@ -0,0 +1,59 @@
+# Fonction pour calculer l'intérêt mensuel
+def calculer_interet_mensuel(capital, taux):
+    return (capital / 100) * taux
+
+# Présentation des règles du calculateur de crédit
+print("Bienvenue dans le calculateur de crédit.")
+print("Règles : Le capital initial doit être supérieur à 500€.")
+print("Le taux d'intérêt mensuel est fixé à 5%.")
+print("Le paiement mensuel est de 100€.")
+
+# Gestion de la saisie utilisateur avec une boucle
+while True:
+    # Entrée utilisateur pour le capital initial
+    capital_initial = input("Entrez le montant souhaité supérieur à 500€) : ")
+
+    # Vérification de la validité de l'entrée
+    try:
+        capital = float(capital_initial)
+        if capital > 500:
+            break  # Sortir de la boucle si l'entrée est valide
+        else:
+            print("Le capital doit être supérieur à 500€. Réessayez.")
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
+
+# Initialisation des variables
+mensualite = 100  # Paiement mensuel
+taux = 5  # Taux d'intérêt mensuel fixé à 5%
+total_interets = 0
+mois = 0
+
+# Affichage du solde initial
+print(f"\nSolde initial : {capital:.2f}€")
+
+# Boucle for pour calculer le nombre de mois de paiement
+for mois in range(1, 500):
+    # Calcul des intérêts et mise à jour du capital
+    interet = calculer_interet_mensuel(capital, taux)
+    capital += interet
+    total_interets += interet
+    capital -= mensualite
+
+    # Vérification si le capital est remboursé
+    if capital <= 0:
+        print(f"\nMois {mois}:")
+        print(f"Payement {capital + interet:.2f}€ moins intérêts {interet:.2f}€")
+        print("Crédit remboursé !")
+        break
+
+    # Affichage des détails mensuels
+    print(f"\nMois {mois}:")
+    print(f"Payement {mensualite}€ moins intérêts {interet:.2f}€")
+    print(f"Solde    {capital:.2f}€")
+
+# Affichage du résultat final
+pourcentage = (total_interets * 100) / float(capital_initial)
+print(f"\nDurée du crédit : {mois} mois ")
+print(f"Coût du crédit : {total_interets:.2f}€")
+print(f"Soit {pourcentage:.2f}% du capital initial.")
