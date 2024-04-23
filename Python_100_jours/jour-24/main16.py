98d868555690af5816b167bdd01e5b6860532fb2
franckdun <scorven7@gmail.com>
2024-04-23 05:17:35 +0200

    update

---
+++ Python_100_jours/jour-24/main16.py
@@ -0,0 +1,78 @@
+import random
+
+# Constantes
+NUM_DICE = 3
+TARGET_SCORE = 421
+
+def display_dice_and_score(dice):
+    """Affiche les résultats des dés et le score."""
+    print("\n", "  ".join([f"Dé {i+1} -> {dice[i]}" for i in range(NUM_DICE)]), "\n")
+    score = sum(dice)
+    print("Nombre obtenu -> ", convert_dice_to_number(dice), "\n")
+    print("Score : +", score, "points\n")
+    return score
+
+def convert_dice_to_number(dice):
+    """Convertit les résultats des dés en un nombre entier."""
+    return int(''.join(map(str, dice)))
+
+def roll_dice():
+    """Simule le lancer de trois dés à six faces."""
+    return sorted([random.randint(1, 6) for _ in range(NUM_DICE)], reverse=True)
+
+def reroll_dice(dice, choices):
+    """Permet au joueur de relancer les dés sélectionnés."""
+    for choice in choices:
+        if choice in range(1, NUM_DICE + 1):
+            dice[choice - 1] = random.randint(1, 6)
+    return sorted(dice, reverse=True)
+
+def play_turn():
+    """Effectue un tour de jeu."""    
+    input("Appuie sur Entrée pour lancer les dés.. ")
+    dice = roll_dice()
+    score = display_dice_and_score(dice)
+    return dice, score
+      
+def play_421():
+    """Fonction principale pour jouer au jeu 421."""
+    total_score = 0
+    num_turns = 0
+
+    print("Bienvenue !\nPour gagner obtiens 421 avec les dés\nou fais le plus de points possible.")
+    print("Faire 'q' pour quitter le jeu.\n")
+
+    while True:
+        num_turns += 1
+        print(f"< NOUVEAU TOUR N°{num_turns} >\n")
+        dice, score = play_turn()
+        total_score += score
+
+        if convert_dice_to_number(dice) == TARGET_SCORE:
+            print("Félicitations ! Tu as obtenu exactement 421 et gagné le jeu !")
+            break
+          
+        choices = input("Quels dés veux-tu relancer ?\n(sépare tes choix par un espace)\nFaire 'all' pour tous les dés \nFaire 'q' pour quiter :").split()
+
+        if "all" in choices:
+          #relance les 3 dés aléatoirement
+            dice = roll_dice()
+          
+        if 'q' in choices:
+            print("\nFin du jeu. Merci d'avoir joué !")
+            break
+
+        dice = reroll_dice(dice, [int(choice) for choice in choices if choice.isdigit()])
+        score = display_dice_and_score(dice)
+        total_score += score
+      
+        if convert_dice_to_number(dice) == TARGET_SCORE :
+            print("Félicitations ! Tu as obtenu exactement 421 et gagné le jeu !")
+            break
+          
+        reroll_dice(dice, choices)
+
+    print(f"\nScore total: {total_score}, Tours joués: {num_turns}")
+
+if __name__ == "__main__":
+    play_421()
